d412cae99652cd69993aeb3be0bd1d15846a281a
s4llm4n <s4llm4n@gmail.com>
2024-10-08 14:25:14 +0700

    responsive page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id='home' className='hero'>
       <img src={profile_img} alt='' />
       <h1><span>I'm Alex Bennett,</span> Frontend developer based in Indonesia</h1>
       <p>I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
         <div className="hero-action">
-            <div className="hero-connect">Connect With Me</div>
+            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
             <div className="hero-resume">My Resume</div>
         </div>  
     </div>
